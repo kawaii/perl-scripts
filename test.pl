42d28ddcb7bf2aca8276809e4a3073a4563abb85
Kane Valentine <kane@cute.im>
2017-04-02 00:03:48 +0100

    upload test

---
+++ test.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+use feature 'switch';
+
+print "Hello World\n";
+
+my $name = 'Kane';
+
+my ($age, $street) = (22, '12 Fake Street');
+
+my $my_info = "$name lives at $street\n";
+
+print $my_info;
